64c8d54bf119fbf6ef569482361e7a79321ea06a
pwrstudio <pwrpntr@gmail.com>
2023-05-29 22:15:48 +0200

    Move content to sequence schema

--- schemas/Sequence.ts
+++ schemas/Sequence.ts
@@ -7,12 +7,44 @@ export default {
     icon: MdListAlt,
     name: "sequence",
     type: "document",
+    groups: [
+        {
+            name: 'en',
+            title: 'English',
+        },
+        {
+            name: 'fr',
+            title: 'French',
+        },
+        {
+            name: 'de',
+            title: 'German',
+        },
+    ],
     fields: [
         {
             title: "Title",
             name: "title",
             type: "string",
             validation: (Rule: any) => Rule.required(),
+        },
+        {
+            title: "Content (English)",
+            name: "content_en",
+            type: "slideEditor",
+            group: ["en"]
+        },
+        {
+            title: "Content (French)",
+            name: "content_fr",
+            type: "slideEditor",
+            group: ["fr"]
+        },
+        {
+            title: "Content (German)",
+            name: "content_de",
+            type: "slideEditor",
+            group: ["de"]
         },
         {
             title: "Images",
@@ -42,6 +74,31 @@ export default {
             ]
         },
     ],
+    preview: {
+        select: {
+            title: 'title',
+            body: 'content_en'
+        },
+        prepare({ title, body }) {
+            let excerpt = ''
+            console.log(body)
+            if (body && body.content && body.content.length > 0) {            // Flatten blocks array to string
+                const plainBody = body.content
+                    .filter(block => block._type === 'block')
+                    .map(block => block.children
+                        .map(child => child.text)
+                        .join(''))
+                    .join('\n')
+
+                excerpt = plainBody.slice(0, 200) + (plainBody.length > 200 ? '...' : '')
+            }
+
+            return {
+                title: title,
+                subtitle: excerpt
+            }
+        }
+    }
 }
 
 
