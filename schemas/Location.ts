8220db63ba1f51c1b63e047b2217b63cb6629cf3
Son La Pham <hello@son-la.co>
2023-08-24 11:21:52 +0200

    location pick habitat

--- schemas/Location.ts
+++ schemas/Location.ts
@@ -42,6 +42,35 @@ export default {
             ]
         },
         {
+            title: 'Habitat',
+            name: 'habitat',
+            type: 'string',
+            description:'Habitat to pull db entries from',
+            options: {
+              list: [
+                'changing-natures.habitats.forest',
+                'changing-natures.habitats.tropical-forest',
+                'changing-natures.habitats.mountains,-rocky-area',
+                'changing-natures.habitats.desert',
+                'changing-natures.habitats.grassland',
+                'changing-natures.habitats.shrubland-/-savannah',
+                'changing-natures.habitats.agricultural-and-pasture-land',
+                'changing-natures.habitats.ocean',
+                'changing-natures.habitats.coastal-zone',
+                'changing-natures.habitats.lake,-pond',
+                'changing-natures.habitats.river',
+                'changing-natures.habitats.wetland',
+                'changing-natures.habitats.human-made-waterbody',
+                'changing-natures.habitats.village',
+                'changing-natures.habitats.city',
+                'changing-natures.habitats.industrial-area',
+                'changing-natures.habitats.park',
+                'changing-natures.habitats.private-or-community-garden',
+                'changing-natures.habitats.home-/-interior'
+              ], 
+            }
+        },
+        {
             title: "Slug",
             name: "slug",
             type: "slug",
